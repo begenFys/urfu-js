8ae8f1636b9ce93df4c26cba242926de9c8183e8
begenFys <vip.nikita.2004@list.ru>
2022-12-28 10:46:44 +0500

    edit dijkstra

--- dijkstra -!/dijkstra.js
+++ dijkstra -!/dijkstra.js
@@ -1,100 +1,107 @@
-let fs = require('fs')
-let arg = process.argv;
-let input = arg[2].toString();
-let str = '';
-let rpn = new Array();
-let stack = new Array();
+function getPostfixExp(exp){
+    let stack = [];
+    let priority = {
+        '(': 0,
+        '+': 1,
+        '-': 1,
+        '*': 2,
+        '/': 2,
+        's': 3,
+    };
+    let buffer = "";
+    let postfixExp = "";
 
-let priority = {
-    '(': 0,
-    ')': 0,
-    '+': 1,
-    '-': 1,
-    '*': 2,
-    '/': 2,
-    's': 3
-};
-
-for (let i = 0; i < input.length; i++) {
-    if (isNaN(input.charAt(i) * 1) && input.charAt(i) != '.'){
-        if (str != ''){
-            rpn.push(str * 1);
+    for (let i = 0; i < exp.length; i++) {
+        if(!isNaN(exp[i]) || exp[i] == '.') {
+            buffer += exp[i];
         }
-        str = '';
-        if (input.charAt(i) == '(') {
-            stack.push('(');
-        }  
-        else if (input.charAt(i) == ')') {
-            while (stack[stack.length - 1] != '(') {
-                rpn.push(stack[stack.length - 1]);
-                stack.pop();
+        if (exp[i] == '(') {
+            stack.push(exp[i]);
+            continue;
+        }
+        if (exp[i] == ')') {
+            postfixExp += buffer + ' ';
+            buffer = "";
+            let ptr = stack.length - 1;
+            while(stack[ptr] != '(') {
+                postfixExp += stack.pop();
+                ptr --;
             }
             stack.pop();
-        } 
-        else if (stack.length == 0 || priority[input.charAt(i)] >= priority[stack[stack.length - 1]]) {
-            stack.push(input.charAt(i))
-        } 
-        else {
-            while (stack[stack.length - 1] != '(' && stack.length) {
-                rpn.push(stack[stack.length - 1]);
-                stack.pop();
+            continue;
+        }
+        if (isNaN(exp[i]) && exp[i] != '.') {
+            postfixExp += buffer + " ";
+            buffer ="";
+            let lastChar = stack[stack.length - 1];
+            if(stack.length != 0 && priority[lastChar] >= priority[exp[i]]) {
+                while(stack.length != 0 && priority[lastChar] >= priority[exp[i]]){
+                    postfixExp += stack.pop() + ' ';
+                    lastChar = stack[stack.length - 1];
+                }
+                
+                stack.push(exp[i]);
             }
-            stack.push(input.charAt(i));
+            else {
+                stack.push(exp[i]);
+            }
+        }
+        if (i == exp.length - 1){
+            postfixExp += buffer + " ";
+            postfixExp += stack.pop();
         }
     }
-    else{
-        str += input.charAt(i);
+    if(stack.length != 0){
+        while(stack.length != 0 ){
+            postfixExp += ' ' + stack.pop();
+        }
     }
-}
-if (str != ''){
-    rpn.push(str * 1);
-}
-while (stack.length) {
-    rpn.push(stack[stack.length - 1]);
-    stack.pop()
+    return postfixExp;
 }
 
-console.log(rpn);
-
-for (let i = 0; i < rpn.length; i++) {
-    if (isNaN(rpn[i]) * 1){
-        if (rpn[i] == '+'){
-            rpn[i - 2] = rpn[i - 2] + rpn[i - 1]; 
-            rpn.splice(i - 1, 2);
-            i -= 2;
-        }
-        else if (rpn[i] == '-'){
-            rpn[i - 2] = rpn[i - 2] - rpn[i - 1];
-            rpn.splice(i - 1, 2);
-            i -= 2;
-        }
-        else if (rpn[i] == '*'){
-            rpn[i - 2] = rpn[i - 2] * rpn[i - 1]; 
-            rpn.splice(i - 1, 2);
-            i -= 2;
-        }
-        else if (rpn[i] == 's'){
-            rpn[i - 2] = rpn[i - 2] ** rpn[i - 1];
-            rpn.splice(i - 1, 2); 
-            i -= 2;
-        }
-        else if (rpn[i] == '/'){
-            rpn[i - 2] = rpn[i - 2] / rpn[i - 1];
-            rpn.splice(i - 1, 2);
-            i -= 2; 
-        }
-        else {
-            break;
-        }
+function calc(operator, a, b) {
+    switch(operator) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+        case 's': return Math.pow(a,b);
     }
 }
 
-console.log(rpn);
-
-for(let i = 0; i < input.length; i++) {
-    input=input.replace("s", "**");
+function getResult(postfixExp){
+    let stack = [];
+    let buffer = "";
+    let a, b;
+    for(let i = 0; i < postfixExp.length; i++) {
+        if(!isNaN(postfixExp[i]) && postfixExp[i] != ' '|| postfixExp[i] == '.') {
+            buffer += postfixExp[i];
+        }
+        if(postfixExp[i] == ' ' && buffer != "") {
+            stack.push(+buffer);
+            buffer = "";
+            continue;
+        }
+        if(postfixExp[i] != ' ' && isNaN(postfixExp[i]) && postfixExp[i] != '.' ) {
+            b = stack.length > 0 ? stack.pop() : 0;
+            a = stack.length > 0 ? stack.pop() : 0;
+            stack.push(calc(postfixExp[i], a, b));
+        }
+    }
+    return stack[0];
 }
 
+let arg = process.argv;
 
-console.log("Наш алгоритм - ", rpn[0]);
-console.log("Ответ eval - ", eval(input));
+let exp = arg[2];
+exp = exp.toString();
+
+let postfixExp = getPostfixExp(exp);
+
+console.log(`Postfix form: ${postfixExp}`);
+let result = getResult(postfixExp);
+console.log(`Result: ${result}`);
+for(let i = 0; i < exp.length; i++) {
+    exp = exp.replace("s", "**");
+}
+console.log(`Eval result: ${eval(exp)}`);
